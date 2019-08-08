9877b214f2cc21b997c048baea55bc15672b0016
meryl <meryl.wimberley@gmail.com>
2019-08-08 16:37:58 -0400

    single-file-component: add data to component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,11 +1,17 @@
 <template lang="html">
   <div>
-    <h4>Event Name Placeholder</h4>
+    <h4> {{ title }}</h4>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: "Event Name Placeholder"
+    }
+  }
+};
 </script>
 
 <style lang="css" scoped></style>
