80d7b65fab0d7e4e1026260c34bba138083c4f5a
meryl <meryl.wimberley@gmail.com>
2019-08-08 12:45:37 -0400

    vue-router-basic: update router.js to use files

--- src/router.js
+++ src/router.js
@@ -1,29 +1,28 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
-import About from "./views/About.vue";
+import EventCreate from "./views/EventCreate.vue";
+import EventList from "./views/EventList.vue";
+import EventShow from "./views/EventShow.vue";
+
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "event-list",
+      component: EventList
     },
     {
-      path: "/about-us",
-      name: "about",
-      component: About
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      //   component: () =>
-      //     import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/event",
+      name: "event-show",
+      component: EventShow,
+      props: true
     },
     {
-      path: "/about",
-      redirect: { name: "about" }
+      path: "/event/create",
+      name: "event-create",
+      component: EventCreate
     }
   ]
 });
