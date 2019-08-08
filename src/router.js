caf2b90903b781ae032153724ff64e0b5cde1252
meryl <meryl.wimberley@gmail.com>
2019-08-08 12:30:01 -0400

    vue-router-basic: test redirecting route path

--- src/router.js
+++ src/router.js
@@ -12,16 +12,18 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
+      path: "/about-us",
       name: "about",
-      component: About,
-      alias: "/about-us"
-
+      component: About
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       //   component: () =>
       //     import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/about",
+      redirect: { name: "about" }
     }
   ]
 });
