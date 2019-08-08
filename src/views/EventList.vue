a41117c6b371f60f373595882be9e7644e270127
meryl <meryl.wimberley@gmail.com>
2019-08-08 15:42:04 -0400

    add-dynamic-route-history: move event-show link to EventList

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,9 @@
 <template lang="html">
   <div>
     <h1>Events Listing</h1>
+    <router-link :to="{ name: 'event-show', params: { id: '1' } }">
+      Show Event #1</router-link
+    >
   </div>
 </template>
 
