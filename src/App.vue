e320d5991d78386c2c6b5bce5df2110a4733a420
meryl <meryl.wimberley@gmail.com>
2019-08-08 12:52:57 -0400

    vue-router-basic: update text to match router name

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link :to="{ name: 'event-list' }">Home</router-link> |
-      <router-link :to="{ name: 'event-show' }">Home</router-link> |
-      <router-link :to="{ name: 'event-create' }">About</router-link>
+      <router-link :to="{ name: 'event-list' }">List</router-link> |
+      <router-link :to="{ name: 'event-show' }">Show Event #1</router-link> |
+      <router-link :to="{ name: 'event-create' }">Create</router-link>
     </div>
     <router-view />
   </div>
