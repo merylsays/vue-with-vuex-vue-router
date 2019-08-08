69d91948934e409658a80f3c2a189acb8478f56f
meryl <meryl.wimberley@gmail.com>
2019-08-08 15:13:07 -0400

    add-dynamic-route-history: accept id prop within EventShow file

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,9 +1,11 @@
 <template lang="html">
-  <h1>Showing Event #1</h1>
+  <h1>Showing Event #{{ id }}</h1>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["id"]
+};
 </script>
 
 <style lang="css" scoped></style>
