5f73115fd5804a7808d0552139431f1a51b65eb1
meryl <meryl.wimberley@gmail.com>
2019-08-08 12:27:10 -0400

    vue-router-basic: test alias redirect example

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-
+import About from "./views/About.vue";
 Vue.use(Router);
 
 export default new Router({
@@ -14,11 +14,14 @@ export default new Router({
     {
       path: "/about",
       name: "about",
+      component: About,
+      alias: "/about-us"
+
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      //   component: () =>
+      //     import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
 });
