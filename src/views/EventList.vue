cbe9de04d4295c82a66f81f5c271686c11e9952a
meryl <meryl.wimberley@gmail.com>
2019-08-08 16:58:13 -0400

    single-file-component: add EventCard to EventList template

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,7 @@
 <template lang="html">
   <div>
     <h1>Events Listing</h1>
+    <EventCard />
     <router-link :to="{ name: 'event-show', params: { id: '1' } }">
       Show Event #1</router-link
     >
