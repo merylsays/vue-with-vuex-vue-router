2dac92fef735b80eda291bb1be27a3d7bac1a3af
meryl <meryl.wimberley@gmail.com>
2019-08-08 17:19:52 -0400

    single-file-component: add scoped styles to EventCard

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,8 +1,10 @@
 <template lang="html">
   <div>
-    <span>@{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
-    <span>{{ event.attendees.length }} attending</span>
+    <div class="event-card">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
   </div>
 </template>
 
@@ -32,7 +34,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h4 {
-  color: #00ff00;
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
 }
 </style>
