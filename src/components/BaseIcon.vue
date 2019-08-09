8d134a2ab66803098006d20d6d938e8e52d2da6a
meryl <meryl.wimberley@gmail.com>
2019-08-09 10:48:24 -0400

    add-global-component: dynamically add svg name, width and height
    using v-bind here which allows us to dynamically add the name of the symbol to the end of this 'xlink:href' path. Add props that allow us to optionally alter width and height.

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -1,9 +1,28 @@
 <template lang="html">
-  <div>Icon</div>
+  <div class="icon-wrapper">
+    <svg class="icon" :width="width" :height="height">
+      <use v-bind="{ 'xlink:href': '/feather-sprite.svg#' + name }" />
+    </svg>
+    <slot></slot>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    //add props to allow optionally altering svg width and height
+    name: String,
+    //the name of the svg symbol I want to use
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
