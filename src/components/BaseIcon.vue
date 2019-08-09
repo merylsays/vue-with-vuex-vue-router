0ee9e0f2bf5d70472824d30748d8e82d573ec712
meryl <meryl.wimberley@gmail.com>
2019-08-09 10:49:33 -0400

    add-global-component: add scoped styles to BaseIcon

--- src/components/BaseIcon.vue
+++ src/components/BaseIcon.vue
@@ -25,4 +25,20 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
