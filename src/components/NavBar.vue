aa2d51eedec1b15604935f7a6d19029fce3d0533
meryl <meryl.wimberley@gmail.com>
2019-08-08 22:54:14 -0400

    top-nav: create route for root path

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="nav" class="nav">
+    <router-link to="/" class="brand">Real World Events</router-link> |
     <nav>
       <router-link :to="{ name: "event-list" }">List</router-link> |
       <router-link :to="{ name: "event-create" }">Create</router-link>
