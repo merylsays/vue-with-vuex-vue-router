cb145bb999e77b6766dc7c98cae5ce8a1fad3a01
meryl <meryl.wimberley@gmail.com>
2019-08-08 22:45:22 -0400

    top-nav: move nav router links from App to NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'event-list' }">List</router-link> |
-      <router-link :to="{ name: 'event-create' }">Create</router-link>
-    </div>
     <router-view />
   </div>
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,10 @@
 <template>
+  <div id="nav" class="nav">
+    <nav>
+      <router-link :to="{ name: "event-list" }">List</router-link> |
+      <router-link :to="{ name: "event-create" }">Create</router-link>
+    </nav>
+  </div>
 </template>
 <script>
 export default {
